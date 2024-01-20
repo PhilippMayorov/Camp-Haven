1d6748d9a2cce92f92d742be02b522409af577fd
PhilippMayorov <philipp.mayorov04@gmail.com>
2024-01-20 17:04:36 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const Campground = require('./models/campground')
+const ejsMate = require('ejs-mate')
+
+mongoose
+  .connect('mongodb://localhost:27017/campHaven')
+  .then(() => {
+    console.log('MONGO: Connection Open!')
+  })
+  .catch((er) => {
+    console.log('ERROR')
+    console.log(er)
+  })
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('once', () => {
+  console.log('DataBase Connected!')
+})
+
+const app = express()
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }))
+
+// name _method does not work
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({})
+  res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/makeCampGround', async (req, res) => {
+  const camp = new Campground({ title: 'My Back yard', price: 40 })
+  await camp.save()
+  res.send(camp)
+})
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground)
+  await campground.save()
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/show', { campground })
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/edit', { campground })
+})
+
+app.post('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  })
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params
+  await Campground.deleteOne({ _id: id })
+  res.redirect('/campgrounds')
+})
+// app.delete('/campgrounds/:id', async (req, res) => {
+//   res.send('DELETED')
+// })
+
+app.listen(3000, () => {
+  console.log('Serving on port 3000')
+})
---
+++ seeds/index.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const Campground = require('../models/campground')
+const cities = require('./cities')
+const { descriptors, places } = require('./seedHelpers')
+
+mongoose
+  .connect('mongodb://localhost:27017/campHaven')
+  .then(() => {
+    console.log('MONGO: Connection Open!')
+  })
+  .catch((er) => {
+    console.log('ERROR')
+    console.log(er)
+  })
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('once', () => {
+  console.log('DataBase Connected!')
+})
+
+const sample = (array) => Math.floor(Math.random() * array.length)
+
+const seedDB = async () => {
+  await Campground.deleteMany({})
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000)
+    const price = Math.floor(Math.floor(Math.random() * 20) + 10)
+
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${descriptors[sample(descriptors)]} ${places[sample(places)]}`,
+      image: 'https://random.imagecdn.app/1280/720',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores dolorum cumque. Sequi, aspernatur magnam quibusdam dicta non pariatur illum optio nesciunt porro quas quos at quidem, exercitationem praesentium numquam.',
+      price: price,
+    })
+    console.log(camp)
+    await camp.save()
+  }
+}
+
+seedDB().then(() => {
+  mongoose.connection.close()
+})
